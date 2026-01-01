5032df04f5b4d6e8b866ef72caf3c25bb4c9d20e
Michal Young <michal.young@gmail.com>
2026-01-01 15:45:23 -0800

    Primary edits, ready for posting.

--- lib/sidebar.php
+++ lib/sidebar.php
@@ -1,7 +1,8 @@
 <!-- sidebar.php:  Sidebar to be replicated on each page 
      Note links must be relative to site URL-->
 <div class="sidenav">
-    <img src="img/handshake.png" width="100%">
+    <img src="img/handshake.png" width="100%"
+    alt="Illustration of robot shaking hands with human">
     <h1>Winter 2026</h1>
     <h2>CS 199</h2>
     <h3>UH 4-5:20</h3>
